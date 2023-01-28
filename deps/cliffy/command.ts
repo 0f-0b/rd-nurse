831b45c0e0445a31440c923bc68bc39ea8b92a51
undefined <sjx233@qq.com>
2023-01-29 00:58:28 +0800

    update deps

--- deps/cliffy/command.ts
+++ deps/cliffy/command.ts
@@ -1,19 +1,23 @@
-import { bold, red } from "https://deno.land/x/cliffy@v0.25.4/command/deps.ts";
+import { bold, red } from "https://deno.land/x/cliffy@v0.25.7/command/deps.ts";
 import {
   Command,
   ValidationError,
-} from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+} from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 
-export * from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
-Object.defineProperty(Command.prototype, "error", {
+export * from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+Object.defineProperty(Command.prototype, "throw", {
   value: {
-    error(this: Command, e: Error): never {
-      if (this.shouldThrowErrors() || !(e instanceof ValidationError)) {
-        throw e;
+    throw(this: Command, error: Error): never {
+      if (error instanceof ValidationError) {
+        error.cmd = this;
+      }
+      this["getErrorHandler"]()?.(error, this);
+      if (!(error instanceof ValidationError) || this.shouldThrowErrors()) {
+        throw error;
       }
       this.showHelp();
-      console.error(`${bold(red("error"))}: ${e.message}`);
+      console.error(`${bold(red("error"))}: ${error.message}`);
       Deno.exit(2);
     },
-  }.error,
+  }.throw,
 });
